58b71edab8b99ae9abfc20ecb138c4d3e59b6dde
jesusegg <jesusegg@hotmail.com>
2021-09-21 15:01:35 -0500

    tracking

--- src/controllers/ChallengeClass.ts
+++ src/controllers/ChallengeClass.ts
@@ -61,12 +61,116 @@ export class Challenge {
   public arrayScore(req: Request, res: Response) {
     const { array }: ArrayChallenge = req.body;
     let output = 0;
-    console.log(dataTalentos, dataTalentosCheckpoint);
     array.forEach((x: number) => {
       if (x % 2 === 0) output += 1;
       if (x % 2 !== 0 && x !== 5) output += 3;
       if (x === 5) output += 5;
     });
     return res.send(`${output}`);
+  }
+
+  public async Tracking(req: Request, res: Response) {
+    const reference = req.params.code;
+
+    const dataEtiquetas = await dataTalentosCheckpoint;
+    const dataGuias = await dataTalentos;
+
+    if (reference.length === 11) {
+      const objGuia = dataGuias.filter(
+        (x: any) => x.codigo_remision === reference
+      )[0];
+      const tickets = objGuia.unidades.map((x: any) => x.etiqueta1d);
+      const objRef = tickets.map((x: any) =>
+        dataEtiquetas.filter((y: any) => y.etiqueta1d === x)
+      );
+
+      const response = {
+        isError: false,
+        status: "success",
+        data: {
+          codigo_remision: reference,
+          nombre_destinatario: objGuia.nombre_destinatario,
+          dir_destinatario: objGuia.dir_destinatario,
+          unidades: [
+            {
+              etiqueta1d: "734380016861001",
+              cantidad_checkpoints: 5,
+              tracking: [
+                {
+                  checkpoint: "Asignacion Para Distribucion",
+                  tipo: "Virtual",
+                },
+                {
+                  checkpoint: "Descargue Ruta Nacional",
+                  tipo: "Fisica",
+                },
+                {
+                  checkpoint: "Despacho",
+                  tipo: "Fisica",
+                },
+                {
+                  checkpoint: "Descargue Recogida",
+                  tipo: "Fisica",
+                },
+                {
+                  checkpoint: "Recogida",
+                  tipo: "Fisica",
+                },
+              ],
+            },
+            {
+              etiqueta1d: "734380016861002",
+              cantidad_checkpoints: 3,
+              tracking: [
+                {
+                  checkpoint: "Asignacion Para Distribucion",
+                  tipo: "Fisica",
+                },
+                {
+                  checkpoint: "Descargue Ruta Nacional",
+                  tipo: "Fisica",
+                },
+                {
+                  checkpoint: "Despacho",
+                  tipo: "Fisica",
+                },
+              ],
+            },
+          ],
+        },
+      };
+      console.log(objRef);
+      return res.json(dataGuias[0]);
+    } else if (reference.length === 15 && reference[0] === "7") {
+      const objGuia = dataGuias.filter((x: any) =>
+        x.unidades.includes(
+          x.unidades.filter((y: any) => y.etiqueta1d === reference)[0]
+        )
+      )[0];
+
+      const objReference = dataEtiquetas.filter(
+        (x: any) => x.etiqueta1d === reference
+      );
+
+      if (!objGuia) return res.send("referencia no encontrada");
+
+      const response = {
+        isError: false,
+        status: "success",
+        data: {
+          etiqueta: reference,
+          informacion_guia: {
+            codigo_remision: objGuia.codigo_remision,
+            nombre_destinatario: objGuia.nombre_destinatario,
+            dir_destinatario: objGuia.dir_destinatario,
+          },
+          cantidad_checkpoints: objReference.length,
+          tracking: objReference,
+        },
+      };
+      return res.json(response);
+    }
+
+    return res.status(404).send("codigo guia o de etiqueta incorrecto");
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import average from "./average";
 import stringManipulation from "./stringManipulation";
 import sumArray from "./sumArray";
 import arrayScore from "./arrayScore";
+import tracking from "./trackingCoordinadora";
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use("/challenge", average);
 routes.use("/challenge", stringManipulation);
 routes.use("/challenge", sumArray);
 routes.use("/challenge", arrayScore);
+routes.use("/challenge", tracking);
 
 export default routes;
